d1752d880e2166fed714134cefcdb8fcc53c154c
Yağmur K. <yagmurrttk@gmail.com>
2024-03-02 17:43:29 +0300

    Add project files.

---
+++ KidKinder/Entities/Adress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KidKinder.Entities
+{
+    public class Adress
+    {
+        public int AdressId { get; set; }
+        public string Description { get; set; }
+        public string AdressDetail { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string OpeningHours { get; set; }
+    }
+}
---
+++ KidKinder/Entities/BookASeat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KidKinder.Entities
+{
+    public class BookASeat
+    {
+        public int BookASeatId { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public int ClassRoomId { get; set; }
+        public virtual ClassRoom ClassRoom { get; set; }
+    }
+}
